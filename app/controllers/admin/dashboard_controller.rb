89c17ecd38d9254560a929f09ca1535b8769b74d
kzcheng <kevinzifancheng@gmail.com>
2020-04-30 13:20:42 -0700

    Complete admin dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
 
   def show
+    @number_of_products = Product.all.length
+    @number_of_categories = Category.all.length
+    pp Product.all.length
   end
 end
