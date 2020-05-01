4f20cd234bb51b88f5bda0a98415aacfdc193da8
kzcheng <kevinzifancheng@gmail.com>
2020-04-30 19:47:12 -0700

    Add display category list feature on admin category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,6 +2,9 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
 
   def index
+    @categories = Category.order(id: :desc).all
+    puts "<<<<<<<<<< OVER HERE >>>>>>>>>>"
+    pp Category.order(id: :desc).all
   end
 
   def new
