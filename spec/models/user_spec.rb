5251cbb75405837dc50dc29536b9ce0b85412c28
kzcheng <kevinzifancheng@gmail.com>
2020-05-04 20:13:01 -0700

    Add password validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,9 +14,32 @@ RSpec.describe User, type: :model do
         "first_name": "Kevin",
         "last_name": "Cheng",
         "email": "kevinzifancheng@gmail.com",
-        "password_digest": "string",
+        "password": "good",
+        "password_confirmation": "good",
       )
       expect(test_user).to be_valid
+    end
+
+    describe "Passwords" do
+      it "is invalid if password is missing" do
+        test_user = User.new(
+          "first_name": "Kevin",
+          "last_name": "Cheng",
+          "email": "kevinzifancheng@gmail.com",
+          "password_confirmation": "good",
+        )
+        expect(test_user).to be_invalid
+      end
+
+      it "is invalid if password_confirmation is missing" do
+        test_user = User.new(
+          "first_name": "Kevin",
+          "last_name": "Cheng",
+          "email": "kevinzifancheng@gmail.com",
+          "password": "good",
+        )
+        expect(test_user).to be_invalid
+      end
     end
   end
 end
