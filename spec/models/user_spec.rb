a7aa52dd5369b74d4f1abe92647cf5a676e9048b
kzcheng <kevinzifancheng@gmail.com>
2020-05-04 19:41:55 -0700

    Create test for creating user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Creation" do
+    it "can create User" do
+      test_user = User.new
+      expect(test_user).to be_an_instance_of(User)
+    end
+  end
+
+  describe "Validations" do
+    # it "is valid with valid attributes" do
+    #   test_product = User.new(
+    #     "name": "test",
+    #     "price": 50,
+    #     "quantity": 50,
+    #     "category": @test_category,
+    #   )
+    #   expect(test_product).to be_valid
+    # end
+  end
 end
