3b4448f5c50cd2581cf8d7b2fcfd39186c428031
kzcheng <kevinzifancheng@gmail.com>
2020-05-04 21:03:45 -0700

    Add validation for first last name, email exsistance

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,28 @@ RSpec.describe User, type: :model do
       expect(test_user.errors.full_messages).to be_empty
     end
 
+    it "is invalid if first name is missing" do
+      test_user = User.new(
+        "last_name": "Cheng",
+        "email": "kevinzifancheng@gmail.com",
+        "password": "good",
+        "password_confirmation": "good",
+      )
+      test_user.validate
+      expect(test_user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it "is invalid if last name is missing" do
+      test_user = User.new(
+        "first_name": "Kevin",
+        "email": "kevinzifancheng@gmail.com",
+        "password": "good",
+        "password_confirmation": "good",
+      )
+      test_user.validate
+      expect(test_user.errors.full_messages).to include("Last name can't be blank")
+    end
+
     describe "Password" do
       it "is invalid if password is missing" do
         test_user = User.new(
@@ -58,38 +80,15 @@ RSpec.describe User, type: :model do
     end
 
     describe "Email" do
-      it "is invalid if password is missing" do
+      it "is invalid if email is missing" do
         test_user = User.new(
           "first_name": "Kevin",
           "last_name": "Cheng",
-          "email": "kevinzifancheng@gmail.com",
+          "password": "good",
           "password_confirmation": "good",
         )
         test_user.validate
-        expect(test_user.errors.full_messages).to include("Password can't be blank")
-      end
-
-      it "is invalid if password_confirmation is missing" do
-        test_user = User.new(
-          "first_name": "Kevin",
-          "last_name": "Cheng",
-          "email": "kevinzifancheng@gmail.com",
-          "password": "good",
-        )
-        test_user.validate
-        expect(test_user.errors.full_messages).to include("Password confirmation can't be blank")
-      end
-
-      it "is invalid if passwords are not the same" do
-        test_user = User.new(
-          "first_name": "Kevin",
-          "last_name": "Cheng",
-          "email": "kevinzifancheng@gmail.com",
-          "password": "good",
-          "password_confirmation": "bad",
-        )
-        test_user.validate
-        expect(test_user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(test_user.errors.full_messages).to include("Email can't be blank")
       end
     end
   end
