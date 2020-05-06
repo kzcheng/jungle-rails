00ff2594f3cefc231062c82d11b643ecf8d6f84c
kzcheng <kevinzifancheng@gmail.com>
2020-05-05 18:51:02 -0700

    comment change

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -19,9 +19,6 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   scenario "They can click a product" do
     visit root_path
 
-    # commented out b/c it's for debugging only
-    # save_and_open_screenshot
-
     expect(page).to have_css "article.product"
 
     page.find("article.product", match: :first).find("header").find("a").find("h4").click
